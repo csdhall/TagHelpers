3187c071448dddd1502035b00fca1cf4e801de27
DESKTOP-1UV35R6\ChanderDhall <me@chanderdhall.com>
2017-02-21 20:00:51 -0600

    All examples are working

---
+++ src/BlogTagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogTagHelpers.Core.Interfaces;
+using BlogTagHelpers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogTagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductManager _productManager;
+        private readonly ICartManager _cartManager;
+
+        public HomeController(IProductManager productManager, ICartManager cartManager)
+        {
+            _productManager = productManager;
+            _cartManager = cartManager;
+        }
+
+        public IActionResult HelloWorld()
+        {
+            return View("HelloWorld");
+        }
+
+        public IActionResult NamingConventions()
+        {
+            return View();
+        }
+
+        public IActionResult AttributeExamples()
+        {
+            return View();
+        }
+
+        public IActionResult SettingContent()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            var productItems = _productManager.GetAll();
+            return View("ProductItems", productItems);
+        }
+
+        public IActionResult Checkout()
+        {
+            var cart = _cartManager.GetCart();
+            return View("Checkout", cart);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/BlogTagHelpers/TagHelpers/ImageTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using BlogTagHelpers.Interfaces;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BlogTagHelpers.TagHelpers
+{
+    [HtmlTargetElement("image", Attributes = ImageName)]
+    public class ImageTagHelper : TagHelper
+    {
+        private const string ImageName = "image-name";
+
+        [HtmlAttributeName(ImageName)]
+        public string Name { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+            output.TagMode = TagMode.SelfClosing;
+            output.Attributes.Clear();
+            output.Attributes.Add("src", $"/images/{Name}-100.png");
+        }
+    }
+}
